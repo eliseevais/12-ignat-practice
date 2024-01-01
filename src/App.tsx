de1f25279a6af4dd526565aec75712e960c7b2f1
Ирина Елисеева <irina.eliseeva1991@gmail.com>
2024-01-02 01:13:41 +0300

    useParams

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {NavLink, Outlet, Route, Routes} from "react-router-dom";
+import {NavLink, Outlet, Route, Routes, useParams} from "react-router-dom";
+
+const Profile = () => {
+  const params = useParams<'*'>();
+  const some = params["*"];
+  console.log(some);
+
+  return (
+    <div>profile</div>
+  )
+}
 
 function App() {
   return (
@@ -9,24 +19,13 @@ function App() {
       <NavLink to={'/'}>main</NavLink>---
       <NavLink to={'/login'}>login</NavLink>---
       <NavLink to={'/profile'}>profile</NavLink>---
-      <NavLink to={'/profile/settings'}>settings</NavLink>
+      <NavLink to={'/profile/1'}>profile/01</NavLink>---
 
       <Routes>
         <Route path={'/*'} element={<div>404</div>}/>
         <Route path={'/'} element={<div>main</div>}/>
         <Route path={'/login'} element={<div>login</div>}/>
-        <Route path={'/profile'} element={(
-          <div>
-            profile
-
-            <Outlet/>
-          </div>
-        )}>
-          <Route path={'/profile/settings'} element={<div>settings</div>}/>
-          <Route path={'*'} element={<div>profile page not found</div>}/>
-          <Route path={':id'} element={<div>id</div>}/>
-          <Route index element={<div>check id</div>}/>
-        </Route>
+        <Route path={'/profile/*'} element={<Profile/>}/>
       </Routes>
     </div>
   );
