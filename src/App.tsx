26aaee3184cbe13043629c191ae87f376853cb64
Ирина Елисеева <irina.eliseeva1991@gmail.com>
2024-01-02 00:55:34 +0300

    nestings

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {NavLink, Outlet, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavLink to={'/'}>main</NavLink>---
+      <NavLink to={'/login'}>login</NavLink>---
+      <NavLink to={'/profile'}>profile</NavLink>---
+      <NavLink to={'/profile/settings'}>settings</NavLink>
+
+      <Routes>
+        <Route path={'/*'} element={<div>404</div>}/>
+        <Route path={'/'} element={<div>main</div>}/>
+        <Route path={'/login'} element={<div>login</div>}/>
+        <Route path={'/profile'} element={(
+          <div>
+            profile
+
+            <Outlet/>
+          </div>
+        )}>
+          <Route path={'/profile/settings'} element={<div>settings</div>}/>
+          <Route path={'*'} element={<div>profile page not found</div>}/>
+          <Route path={':id'} element={<div>id</div>}/>
+          <Route index element={<div>check id</div>}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
